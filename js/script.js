22911ae26706be7b753cd561ef9518ffb1baaa0b
Luiz Santos <luizcsbh@gmail.com>
2020-05-08 21:38:47 -0300

    Atualizando o portal

--- js/script.js
+++ js/script.js
@@ -1,65 +1,176 @@
-const app = document.getElementById('root')
+let rooms = [];
+let roomTypes = [];
+let totalPages = 1;
+let itemsPerPage = 8;
+let currentPage = 1;
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+function loadDataFromApi() {
+  fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72')
+  .then(response => response.json())
+  .then(data => {
+    rooms = data.map(item => {
+      if (!roomTypes.includes(item.property_type)) {
+        roomTypes.push(item.property_type);
+      }
+      item.formattedPrice = formatValue(item.price);
+      return item;
+    });
+    totalPages = Math.ceil(rooms.length / itemsPerPage);
+    renderFilters();
+    changePage(1);
+  })
+  .catch(err => {
+  })
+}
 
-app.appendChild(container)
+function formatValue(value) {
+  const formattedValue = new Intl.NumberFormat('pt-BR', {
+    style: 'currency', 
+    currency: 'BRL' }
+  ).format(value);
+  return formattedValue;
+}
 
-let request = new XMLHttpRequest()
-request.open('GET', 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72', true)
-request.onload = function() {
-  // Iniciando acesso aos dados JSON
-  let data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(post => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+function renderFilters() {
 
-      const img = document.createElement('img')
-      img.src = post.photo
+  let filterTypes = '';
+  roomTypes.map((type, index) => {
+    filterTypes += `
+    <a href="javascript:filterType(${index})" class="filter__button filter-${index}">${type}</a>
+    `;
+  })
+  const filtersHtml = `
+    <a href="javascript:filterPrice('smaller')" class="filter__button smaller">Menor Preço</a>
+    <a href="javascript:filterPrice('bigger')" class="filter__button bigger">Maior Preço</a>
+    ${filterTypes}
+    <a href="/" class="filter__button clear hide">Limpar</a>
+  `;
 
-      const h3 = document.createElement('h3')
-      h3.textContent = `Tipo de acomodação: ${post.property_type}`
+  const containerFilters = document.getElementById('container__filters');
+  containerFilters.innerHTML = filtersHtml;
+}
 
-      const p = document.createElement('p')
-      post.name = post.name.substring(0, 300)
-      p.textContent = `Descrição: ${post.name}...`
-
-      const span = document.createElement('span')
-      span.textContent = `R$${post.price}/noite`
-
-      const h4 = document.createElement('h4')
-      h4.textContent = `Total R$ `
-
-
-      /**Adicionando elementos html*/
-      container.appendChild(card)
-      card.appendChild(img)
-      card.appendChild(h3)
-      card.appendChild(p)
-      card.appendChild(span)
-      card.appendChild(h4)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
+function cleanFilterActive() {
+  const filters = document.querySelectorAll('.filter__button');
+  for (i = 0; i < filters.length; ++i) {
+    filters[i].classList.remove('active');
   }
 }
 
-request.send();
+function sortItems(items, field, order) {
+  if (order === 'asc') {
+    items.sort(function (a, b) {
+      if (a[field] > b[field]) {
+        return 1;
+      }
+      if (a[field] < b[field]) {
+        return -1;
+      }
+      return 0;
+    });
+  } else {
+    items.sort(function (a, b) {
+      if (a[field] < b[field]) {
+        return 1;
+      }
+      if (a[field] > b[field]) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+  return items;
+}
 
-/**Função valida data */
-function validaCalendar(){
+function filterPrice(type) {
+  cleanFilterActive();
+  let roomsSort = [...rooms];
+  if (type === 'smaller') {
+    rooms = sortItems(roomsSort, 'price', 'asc');
+  } else {
+    rooms = sortItems(roomsSort, 'price', 'desc');
+  }
+
+  changePage(1);
+
+  const filter = document.querySelector(`.filter__button.${type}`);
+  filter.classList.add('active');
+  const clear = document.querySelector('.filter__button.clear');
+  clear.classList.remove('hide');
+}
+
+function filterType(index) {
+  cleanFilterActive();
+  if (roomTypes[index]) {
+    const filtered = rooms.filter(room => room.property_type === roomTypes[index]);
+    renderRooms(filtered);
+
+    const filter = document.querySelector(`.filter__button.filter-${index}`);
+    filter.classList.add('active');
+    const clear = document.querySelector('.filter__button.clear');
+    clear.classList.remove('hide');
+  }
+}
+
+function renderRooms(roomsPage, page) {
+  const containerRooms = document.getElementById('container__rooms');
+  
+  containerRooms.innerHTML = '';
+
+  roomsPage.map(room => {
+    const roomHtml = `
+    <article class="room">
+      <figure>
+        <img src="${room.photo}" alt="${room.name}">
+      </figure>
+      <label class="room__type">${room.property_type}</label>
+      <p class="room__title">${room.name}</p>
+      <p class="room__price"><strong>${room.formattedPrice}</strong>/noite</p>
+    </article>
+    `;
+    containerRooms.insertAdjacentHTML('beforeend', roomHtml);
+  });
+
+  if (page) {
+    renderPagination(page);
+  } else {
+    const containerPagination = document.getElementById('container__pagination');
+    containerPagination.innerHTML = '';
+  }
+}
+
+function renderPagination(page) {
+  let itemsPages = '';
+  for (let i = 1; i <= totalPages; i++) {
+    itemsPages += `<a class="${page == i ? 'active' : ''}" href="javascript:changePage(${i})">${i}</a>`;
+  }
+  const paginationHtml = `
+    <div class="pagination">
+      ${itemsPages}
+    </div>
+  `;
+  const containerPagination = document.getElementById('container__pagination');
+  containerPagination.innerHTML = '';
+  containerPagination.insertAdjacentHTML('beforeend', paginationHtml);
+}
+
+function changePage(page) {
+  currentPage = page;
+  let roomsPage = [...rooms];
+  roomsPage = roomsPage.splice((page - 1) * itemsPerPage, itemsPerPage);
+  renderRooms(roomsPage, page);
+}
+
+function validaCalendar() {
   let today = new Date();
   let dd = today.getDate();
   let mm = today.getMonth()+1; //January is 0!
   let yyyy = today.getFullYear();
-  if(dd<10){
-          dd='0'+dd
+  if(dd < 10) {
+          dd = '0' + dd
       } 
-      if(mm<10){
-          mm='0'+mm
+      if(mm < 10) {
+          mm = '0' + mm
       } 
   today = yyyy+'-'+mm+'-'+dd;
   /**Data de check-in não pode ser inferior a data atual */
@@ -69,24 +180,16 @@ function validaCalendar(){
   
 }/**Fim da função ValidaCalendar */
 
-/**Calcula Preço Total */
-function precoTotal(diarias,){
-
-}
-/** Fim função Calcula Preço Total */
-
 /**Calcula Perído */
 function calculaPeriodo() {
-  let numero = document.getElementById('numeroHospedes').value;
-  let dataEntrada = new Date(document.getElementById('calendar-in').value);
-  let dataSaida = new Date (document.getElementById('calendar-out').value);
+  console.log('Iniciando o calculo do período');
+  let dataEntrada = new Date(document.getElementById('calendar-in').value); //Data de Check-in
+  let dataSaida = new Date (document.getElementById('calendar-out').value); //Data de Check-out
 
-  let diarias = dataSaida.getDate() - dataEntrada.getDate();
-  
-  if(diarias === 0){
-    console.log("Diarias inferior a " +diarias+ " dia.")
-    diarias+=1;
-    console.log("Trabalhamos apenas com dias de um dia.") 
-  }
-  
+  let diff = Math.abs(dataSaida.getTime() - dataEntrada.getTime()); //Subtrai uma data pela outr
+  let onedays =  1000 * 60 * 60 * 24;
+
+  return Math.ceil(diff / onedays);
 }
+
+loadDataFromApi();
